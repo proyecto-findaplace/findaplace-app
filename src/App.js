da7d2d7cd436d29ce21c0f942c5c950601b5db6e
furenku <furenku@gmail.com>
2018-10-09 22:15:48 -0500

    estructura basica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PlaceList from './components/Places/PlaceList/PlaceList';
+import EventNew from './components/Events/EventNew/EventNew';
+import EventList from './components/Events/EventList/EventList';
+
+import { Route, Link } from 'react-router-dom';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+        <header id="main-header">
+          <nav>
+            <ul>
+              <Link to="/">
+                <li>Lugares</li>
+              </Link>
+              <Link to="/eventos">
+                <li>Eventos</li>
+              </Link>
+              <Link to="/evento-nuevo">
+                <li>Evento Nuevo</li>
+              </Link>
+
+            </ul>
+          </nav>
         </header>
+
+        <Route exact path="/" component={PlaceList}/>
+        <Route exact path="/eventos" component={EventList}/>
+        <Route exact path="/evento-nuevo" component={EventNew}/>
+        
       </div>
     );
   }
